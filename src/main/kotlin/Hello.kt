34d0326d15cfb74b97cd30333277ff014afa835a
adam.su <kwangshien@gmail.com>
2018-12-25 22:05:11 +0800

    Initial commit

---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,14 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//    println("KT Hello World!!")
+    val h = Human()
+    h.hello()
+}
+
+
+class Human{
+    fun hello(){
+        println("Hello KT~~")
+    }
+}
